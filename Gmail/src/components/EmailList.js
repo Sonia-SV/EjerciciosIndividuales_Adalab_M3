0c0780bbe8952432179ceb7af128755e51ec3f15
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-07-28 12:32:42 +0200

    Update Module 3

--- Gmail/src/components/EmailList.js
+++ Gmail/src/components/EmailList.js
@@ -2,14 +2,25 @@ import React from 'react';
 import EmailItem from './EmailItem';
 
 class EmailList extends React.Component {
+  renderEmails() {
+    return this.props.emails.map((email, index) => {
+      return (
+        <EmailItem
+          key={index}
+          id={email.id}
+          from={email.from}
+          time={email.time}
+          subject={email.subject}
+          deleteEmail={this.props.deleteEmail}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <table className="table">
-        <tbody>
-          <EmailItem />
-          <EmailItem />
-          <EmailItem />
-        </tbody>
+        <tbody>{this.renderEmails()}</tbody>
       </table>
     );
   }
