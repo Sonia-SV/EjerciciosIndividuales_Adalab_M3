7bf10dcf50c79197e18573fd8f49830f11129922
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-07-31 19:14:08 +0200

    Add exercise 1, lesson 11

---
+++ Módulo 3-lección-11/src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Pricing from './Pricing';
+import '../stylesheets/App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <nav>
+            <ul className="menu">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/pricing">Pricing</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/pricing" component={Pricing} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Módulo 3-lección-11/src/components/Pricing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Pricing extends React.Component {
+  render() {
+    return (
+      <div className="Pcontenedor">
+        <h1>PRICING</h1>
+      </div>
+    );
+  }
+}
+
+export default Pricing;
